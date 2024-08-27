d50e50bd2e182915ac4fb71f94b02d73cc726055
TwitchGG <twitchgg@yahoo.com>
2024-08-27 13:54:52 +0800

    add neovide options
    1. fallback font
    2. window blur
    ...

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,24 @@
 -- Add any additional options here
 local opt = vim.opt
 opt.mouse = "" -- Disable mouse
+
+if vim.g.neovide then
+  opt.guifont = "JetBrainsMono Nerd Font,LXGW WenKai:h11"
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_underline_stroke_scale = 1.0
+  vim.g.neovide_theme = 'auto'
+  vim.g.neovide_no_idle = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_smooth_blink = false
+end

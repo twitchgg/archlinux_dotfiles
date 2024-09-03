4591fdd940d32066977c52249da1a4f3e15647fd
TwitchGG <twitchgg@yahoo.com>
2024-09-03 22:39:10 +0800

    update

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,8 +6,18 @@ opt.mouse = "a"
 
 if vim.g.neovide then
   opt.guifont = "JetBrainsMono Nerd Font,LXGW WenKai:h11"
-  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+  end
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
   vim.g.neovide_window_blurred = true
+  vim.g.neovide_window_floating_blur = 0.8
+  vim.g.neovide_window_floating_opacity = 0.8
+  vim.g.neovide_floating_transparency = 1
+  vim.g.neovide_floating_blur = 0.8
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
   vim.g.neovide_floating_shadow = true

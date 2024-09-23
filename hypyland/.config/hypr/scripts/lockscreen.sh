54009406643be126ec2cae27ece6274226f114a5
TwitchGG <twitchgg@yahoo.com>
2024-09-23 22:51:42 +0800

    update

--- hypyland/.config/hypr/scripts/lockscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Script for lockscreen
-
-sleep 2
-powerprofilesctl set power-saver
-hyprlock -c ~/.config/hypr/hyprlock/hyprlock.conf
-powerprofilesctl set performance
--- hypyland/.config/hypr/scripts/waybarRestart.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-#Restart Waybar and swaync
-
-killall waybar
-killall swaync
-waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
-swaync -s ~/.config/swaync/style.css -c ~/.config/swaync/config.json &
-notify-send --app-name=HOME -i ~/.config/fastfetch/moon.png Hello

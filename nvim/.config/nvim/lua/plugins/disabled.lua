4090305da703dc1a34776f3e5f1f2e3c8d9e9399
TwitchGG <twitchgg@yahoo.com>
2024-12-05 10:32:53 +0800

    update

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,9 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",            enabled = false },
   -- { "iamcco/markdown-preview.nvim", enabled = false },
   -- { "MeanderingProgrammer/markdown.nvim", enabled = false },
-  { "serenevoid/kiwi.nvim",        enabled = false },
-  { "OXY2DEV/markview.nvim",       enabled = false },
+  { "serenevoid/kiwi.nvim",                   enabled = false },
+  { "OXY2DEV/markview.nvim",                  enabled = false },
+  { "rachartier/tiny-inline-diagnostic.nvim", enabled = false },
+
 }

fff507edb8ab4d7267d25ea7e2f1032ab1082a5f
TwitchGG <twitchgg@yahoo.com>
2024-12-05 10:33:20 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,20 +5,21 @@ end)
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.initial_cols = 180
-config.initial_rows = 47
+config.initial_cols = 200
+config.initial_rows = 50
 
-config.color_scheme = "catppuccin-macchiato"
+-- config.color_scheme = "catppuccin-macchiato"
 
 config.line_height = 1.0
-config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", { family = "LXGW WenKai", scale = 1 } })
-config.font_size = 12
+config.font = wezterm.font_with_fallback({ { family = "JetBrainsMono Nerd Font", scale = 1, weight = "DemiBold" }, { family = "LXGW WenKai", scale = 1 } })
+config.font_size = 17
 
 config.use_fancy_tab_bar = false
 config.show_tabs_in_tab_bar = true
 config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.hide_mouse_cursor_when_typing = true
+config.color_scheme = "Catppuccin Mocha"
 
 config.visual_bell = {
   fade_in_function = "EaseIn",
@@ -27,16 +28,16 @@ config.visual_bell = {
   fade_out_duration_ms = 150,
 }
 
-config.colors = {
-  visual_bell = "#202020",
-  -- Make the selection text color fully transparent.
-  -- When fully transparent, the current text color will be used.
-  selection_fg = 'none',
-  -- Set the selection background color with alpha.
-  -- When selection_bg is transparent, it will be alpha blended over
-  -- the current cell background color, rather than replace it
-  selection_bg = 'rgba(50% 50% 50% 50%)',
-}
+-- config.colors = {
+--   visual_bell = "#202020",
+--   -- Make the selection text color fully transparent.
+--   -- When fully transparent, the current text color will be used.
+--   selection_fg = 'none',
+--   -- Set the selection background color with alpha.
+--   -- When selection_bg is transparent, it will be alpha blended over
+--   -- the current cell background color, rather than replace it
+--   selection_bg = 'rgba(50% 50% 50% 50%)',
+-- }
 
 config.visual_bell = {
   fade_in_duration_ms = 75,
@@ -45,7 +46,17 @@ config.visual_bell = {
 }
 
 config.default_cursor_style = "BlinkingBlock"
-config.window_background_opacity = 1
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
+config.window_decorations = "NONE"
 config.text_background_opacity = 1
+
+config.keys = {
+  {
+    key = 'f',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
 
 return config
